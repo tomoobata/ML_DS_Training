3728607c9859652496aadd94ba7054a213f7c7c1
tomoobata <timo77@antioch.jp>
2016-01-23 21:51:52 +0900

    add code of chapter3,  list1

--- ch3_1.py
+++ ch3_1.py
@@ -16,7 +16,7 @@ target_path = sys.argv[2]
 # テンプレート、対象画像の読み込み（グレースケール）
 template = io.imread(template_path, as_gray = True)
 target = io.imread(target_path, as_gray = True)
-th, tw = template.shape
+th, tw, dmmy = template.shape
 
 # (3)
 score_map = np.zeros( shape = (target.shape[0]-th, target.shape[1]-tw))
@@ -25,9 +25,28 @@ score_map = np.zeros( shape = (target.shape[0]-th, target.shape[1]-tw))
 for y in range(score_map.shape[0]):
     for x in range(score_map.shape[1]):
         diff = target[y:y + th, x:x + tw] - template
-        score_map[y, x] = np.squre(diff).sum()
+        score_map[y, x] = np.square(diff).sum()
 
 # (5) SSD(差の二乗和)が最小の座標を取得
-x, y = np.unravel_index(np.argmin(score_map), socre_map.shape)
+x, y = np.unravel_index(np.argmin(score_map), score_map.shape)
 
 # (6) 結果を可視化
+fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(8,3))
+ax1.imshow(template, cmap=cm.Greys_r)
+ax1.set_axis_off()
+ax1.set_title('template')
+
+ax2.imshow(target, cmap=cm.Greys_r)
+ax2.set_axis_off()
+ax2.set_title('target')
+# マッチした領域を短形で囲う
+# ax2.add_patch(plt.Rectangle((y,x),tw,th, edgecolor='w', facecolor='none',linewidth=2.5))
+rect = plt.Rectangle((y,x),tw,th, edgecolor='w', facecolor='none',linewidth=2.5)
+ax2.add_patch(rect)
+
+ax3.imshow(score_map, cmap=cm.Greys_r)
+ax3.set_axis_off()
+ax3.set_title('score_map')
+# マッチした領域を短形で囲う
+ax3.add_patch(plt.Rectangle((y-th/2,x-tw/2),tw,th, edgecolor='w', facecolor='none',linewidth=2.5))
+plt.show()
